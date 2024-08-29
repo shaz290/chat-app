4497352377b4c0d01d5c5ebed83d5e41f332163c
shaz.ahmed <steve.josh@kanrad.com>
2024-08-29 12:33:55 +0530

    route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login/Login'
+import Chat from './pages/Chat/Chat'
+import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/Chat' element={<Chat />} />
+        <Route path='/profile' element={<ProfileUpdate />} />
+
+      </Routes>
+    </>
   )
 }
 
