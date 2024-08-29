92f1dab87ae10bcd7d04d70c3f22f94d7fd38347
shaz.ahmed <steve.josh@kanrad.com>
2024-08-29 16:07:04 +0530

    top chat section

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './ChatBox.css'
+import assets from '../../assets/assets'
+
 
 const ChatBox = () => {
     return (
-        <div>ChatBox</div>
+        <div className='chat-box'>
+            <div className="chat-user">
+                <img src={assets.profile_img} alt="" />
+                <p>Ricky NewUser <img className='dot' src={assets.green_dot} alt="" /></p>
+                <img src={assets.help_icon} className='help' alt="" />
+            </div>
+        </div>
     )
 }
 
