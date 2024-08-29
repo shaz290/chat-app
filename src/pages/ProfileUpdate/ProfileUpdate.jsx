15c543ec4ad83b2133cd3b5138f16d7e5e57c705
shaz.ahmed <steve.josh@kanrad.com>
2024-08-29 18:38:42 +0530

    profileupdate complete

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProfileUpdate.css'
 import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
+
+    const [image, setImage] = useState(false);
+
     return (
         <div className='profile'>
             <div className="profile-container">
                 <form >
                     <h3>Profile Details</h3>
                     <label htmlFor="avatar">
-                        <input type="file" id="avatar" accept='.png,.jpg,.jpeg' hidden />
-                        <img src={assets.avatar_icon} alt="" />
+                        <input onChange={(e) => setImage(e.target.files[0])} type="file" id="avatar" accept='.png,.jpg,.jpeg' hidden />
+                        <img src={image ? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
                         Upload Profile Image
                     </label>
                     <input type="text" placeholder='Your name' required />
                     <textarea placeholder='Write profile bio' required></textarea>
                     <button type='submit'>Save</button>
                 </form>
-                <img src={assets.logo_icon} alt="" />
+                <img className='profile-pic' src={image ? URL.createObjectURL(image) : assets.logo_icon} alt="" />
             </div>
         </div>
     )
