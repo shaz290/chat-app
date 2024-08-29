b277431cbbbec225e68b9efb79dadfc9cc0509cb
shaz.ahmed <steve.josh@kanrad.com>
2024-08-29 17:41:49 +0530

    profile update jsx

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
     return (
-        <div>ProfileUpdate</div>
+        <div className='profile'>
+            <div className="profile-container">
+                <form >
+                    <h3>Profile Details</h3>
+                    <label htmlFor="avatar">
+                        <input type="file" id="avatar" accept='.png,.jpg,.jpeg' hidden />
+                        <img src={assets.avatar_icon} alt="" />
+                        Upload Profile Image
+                    </label>
+                    <input type="text" placeholder='Your name' required />
+                    <textarea placeholder='Write profile bio' required></textarea>
+                    <button type='submit'>Save</button>
+                </form>
+                <img src={assets.logo_icon} alt="" />
+            </div>
+        </div>
     )
 }
 
