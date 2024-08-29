e78781dc6f1ce0275496f416627c29d317644b22
shaz.ahmed <steve.josh@kanrad.com>
2024-08-29 16:25:03 +0530

    chat part complete

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -11,6 +11,35 @@ const ChatBox = () => {
                 <p>Ricky NewUser <img className='dot' src={assets.green_dot} alt="" /></p>
                 <img src={assets.help_icon} className='help' alt="" />
             </div>
+            <div className="chat-message">
+                <div className="s-msg">
+                    <p className="msg">
+                        hjbxjda h quxdhvcb uqhkxbcn hqjcvdbedeeeeeeeeeeeeeeeeeeec33333333333333333333
+                    </p>
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:20 PM</p>
+                    </div>
+                </div>
+                <div className="s-msg">
+                    <img className='msg-img' src={assets.pic1} alt="" />
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:20 PM</p>
+                    </div>
+                </div>
+
+                <div className="r-msg">
+                    <p className="msg">
+                        hjbxjda h quxdhvcb uqhkxbcn hqjcvdbedeeeeeeeeeeeeeeeeeeec33333333333333333333
+                    </p>
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:20 PM</p>
+                    </div>
+                </div>
+            </div>
+
             <div className="chat-input">
                 <input type="text" placeholder='Send a message' />
                 <input type="file" id='image' accept='image/png,image/jpeg' hidden />
