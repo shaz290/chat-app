cd11fa850aca6684c025008e898f6a175d90e754
shaz.ahmed <steve.josh@kanrad.com>
2024-08-30 15:02:44 +0530

    upload profile pic

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,23 +1,95 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './ProfileUpdate.css'
 import assets from '../../assets/assets'
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth, db } from '../../config/firebase';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import upload from '../../lib/Upload';
 
 const ProfileUpdate = () => {
 
+
+    const navigate = useNavigate();
     const [image, setImage] = useState(false);
+    const [name, setName] = useState("");
+    const [bio, setBio] = useState("");
+    const [uid, setUid] = useState("");
+    const [prevImage, setPrevImage] = useState("");
+
+    const profileUpdate = async (event) => {
+        event.preventDefault();
+        try {
+            if (!prevImage && !image) {
+                toast.error("Upload profile picture");
+                return;
+            }
+
+            const docRef = doc(db, 'users', uid);
+
+            if (image) {
+                const imageUrl = await upload(image); // Await the upload function
+                setPrevImage(imageUrl);
+
+                await updateDoc(docRef, {
+                    avatar: imageUrl,
+                    bio: bio,
+                    name: name
+                });
+            } else {
+                await updateDoc(docRef, {
+                    bio: bio,
+                    name: name
+                });
+            }
+
+            toast.success("Profile updated successfully!");
+        } catch (error) {
+            toast.error("Failed to update profile. Please try again.");
+            console.error("Error updating profile:", error);
+        }
+    };
+
+
+
+    useEffect(() => {
+        onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                setUid(user.uid);
+                const docRef = doc(db, "users", user.uid);
+                const docSnap = await getDoc(docRef);
+                if (docSnap.data().name) {
+                    setName(docSnap.data().name)
+                }
+
+                if (docSnap.data().bio) {
+                    setBio(docSnap.data().bio)
+                }
+                if (docSnap.data().avatar) {
+                    setPrevImage(docSnap.data().avatar)
+                }
+            }
+
+            else {
+                navigate('/')
+            }
+        })
+
+    }, [])
 
     return (
         <div className='profile'>
             <div className="profile-container">
-                <form >
+                <form onSubmit={profileUpdate} >
                     <h3>Profile Details</h3>
                     <label htmlFor="avatar">
                         <input onChange={(e) => setImage(e.target.files[0])} type="file" id="avatar" accept='.png,.jpg,.jpeg' hidden />
                         <img src={image ? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
                         Upload Profile Image
                     </label>
-                    <input type="text" placeholder='Your name' required />
-                    <textarea placeholder='Write profile bio' required></textarea>
+                    <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder='Your name' required />
+                    <textarea onChange={(e) => setBio(e.target.bio)} value={bio} placeholder='Write profile bio' required></textarea>
                     <button type='submit'>Save</button>
                 </form>
                 <img className='profile-pic' src={image ? URL.createObjectURL(image) : assets.logo_icon} alt="" />
